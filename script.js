5e58e18c3c3fd0a2631b58c51291efdbf8ed2415
moproblems8 <mhasan1080@gmail.com>
2023-07-04 17:16:33 -0700

    navbar done, i'll add some more style now

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
+const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
+const navBarLinks = document.getElementsByClassName('navbar-links')[0];
 
-class Book {
-    constructor(title, author, isbn, publicationYear, description) {
-        this.title = title;
-        this.author = author;
-        this.description = description;
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() { // The "DOMContentLoaded" event is fired by the browser when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
-    // Get the form element
-    var bookForm = document.getElementById('bookForm');
-
-    // Add submit event listener to the form
-    bookForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        // Get the form values
-        var title = document.getElementById('title').value;
-        var author = document.getElementById('author').value;
-        var description = document.getElementById('description').value;
-
-        // Create a new Book instance
-        var book = new Book(title, author, isbn, publicationYear, description);
-
-
-        // Do something with the book data (e.g., store it, display it, etc.)
-        console.log(book.title);
-
-        // Clear the form fields
-    });
+toggleButton.addEventListener('click', function() {
+    return navBarLinks.classList.toggle('active');
 });
+
+
+// class Book {
+//     constructor(title, author, isbn, publicationYear, description) {
+//         this.title = title;
+//         this.author = author;
+//         this.description = description;
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', function() { // The "DOMContentLoaded" event is fired by the browser when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
+//     // Get the form element
+//     var bookForm = document.getElementById('bookForm');
+
+//     // Add submit event listener to the form
+//     bookForm.addEventListener('submit', function(event) {
+//         event.preventDefault();
+
+//         // Get the form values
+//         var title = document.getElementById('title').value;
+//         var author = document.getElementById('author').value;
+//         var description = document.getElementById('description').value;
+
+//         // Create a new Book instance
+//         var book = new Book(title, author, isbn, publicationYear, description);
+
+
+//         // Do something with the book data (e.g., store it, display it, etc.)
+//         console.log(book.title);
+
+//         // Clear the form fields
+//     });
+// });
