d1084718e90bf32271f460e82caf3aaca36ecba8
moproblems8 <mhasan1080@gmail.com>
2023-07-05 21:57:04 -0700

    updated some code to be more concise

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+'use strict'
+document.addEventListener('DOMContentLoaded', function() {
+
 // navbar toggle button ------------------------------------------
 const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
 const navBarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -12,55 +15,57 @@ const bookCardForm = document.getElementById('book-card-wrapper');
 const blurBackground = document.getElementById('blur');
 let isFormOpen = false;
 
-addBookButton.addEventListener('click', function () {
-  if (isFormOpen) {
-    bookCardForm.classList.remove('active');
-    blurBackground.classList.remove('active');
-    isFormOpen = false;
-  } else {
+function openForm() {
     bookCardForm.classList.add('active');
     blurBackground.classList.add('active');
     isFormOpen = true;
-  }
-});
+}
 
-
-window.addEventListener("click", function (event) {
-  if (event.target !== addBookButton && !bookCardForm.contains(event.target)) {
+function closeForm() {
     bookCardForm.classList.remove('active');
     blurBackground.classList.remove('active');
     isFormOpen = false;
+    bookCardForm.reset();
+}
+
+addBookButton.addEventListener('click', function () {
+  if (isFormOpen) {
+    closeForm();
+  } else {
+    openForm();
   }
 });
 
-// class Book {
-//     constructor(title, author, isbn, publicationYear, description) {
-//         this.title = title;
-//         this.author = author;
-//         this.description = description;
-//     }
-// }
+window.addEventListener("click", function (event) {
+  if (event.target !== addBookButton && !bookCardForm.contains(event.target)) {
+    closeForm();
+  }
+});
 
-// document.addEventListener('DOMContentLoaded', function() { // The "DOMContentLoaded" event is fired by the browser when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
-//     // Get the form element
-//     var bookForm = document.getElementById('bookForm');
+// book card form -----------------------------------------------
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
 
-//     // Add submit event listener to the form
-//     bookForm.addEventListener('submit', function(event) {
-//         event.preventDefault();
+// Add submit event listener to the form
+bookCardForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-//         // Get the form values
-//         var title = document.getElementById('title').value;
-//         var author = document.getElementById('author').value;
-//         var description = document.getElementById('description').value;
+    const title = document.getElementById('form-title').value;
+    const author = document.getElementById('form-author').value;
+    const pages = document.getElementById('form-pages').value;
+    
 
-//         // Create a new Book instance
-//         var book = new Book(title, author, isbn, publicationYear, description);
-
-
-//         // Do something with the book data (e.g., store it, display it, etc.)
-//         console.log(book.title);
-
-//         // Clear the form fields
-//     });
-// });
+    console.log('Form submitted');
+    console.log('Title:', title);
+    console.log('Author:', author);
+    console.log('Pages:', pages);
+    
+    // Reset the form
+    bookCardForm.reset();
+});
+});
